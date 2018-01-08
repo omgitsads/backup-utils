0c35f217b9bbb5e45f2ea8d17dbdeab66938e622
Colin Seymour <colin@github.com>
2018-01-08 19:18:03 +0000

    Put back timestamp export

--- test/test-ghe-detect-leaked-ssh-keys.sh
+++ test/test-ghe-detect-leaked-ssh-keys.sh
@@ -49,7 +49,7 @@ begin_test "ghe-detect-leaked-ssh-keys leaked keys in current backup"
   echo 98:d8:99:d3:be:c0:55:05:db:b0:53:2f:1f:ad:b3:60 >> "$SHARED_UTILS_PATH/ghe-ssh-leaked-host-keys-list.txt"
   cat "$SHARED_UTILS_PATH/ghe-ssh-leaked-host-keys-list.txt"
 
-  #export GHE_SNAPSHOT_TIMESTAMP=1
+  export GHE_SNAPSHOT_TIMESTAMP=1
   ghe-detect-leaked-ssh-keys -s "$GHE_DATA_DIR/1" | grep "Leaked key found in current backup snapshot"
 )
 end_test
@@ -65,7 +65,7 @@ begin_test "ghe-detect-leaked-ssh-keys leaked keys in old snapshot"
   echo 98:d8:99:d3:be:c0:55:05:db:b0:53:2f:1f:ad:b3:60 >> "$SHARED_UTILS_PATH/ghe-ssh-leaked-host-keys-list.txt"
   cat "$SHARED_UTILS_PATH/ghe-ssh-leaked-host-keys-list.txt"
 
-  #export GHE_SNAPSHOT_TIMESTAMP=2
+  export GHE_SNAPSHOT_TIMESTAMP=2
   output=$(ghe-detect-leaked-ssh-keys -s "$GHE_DATA_DIR/2")
   ! echo $output | grep -q "Leaked key in current backup"
   echo $output | grep -q "One or more older backup snapshots"
